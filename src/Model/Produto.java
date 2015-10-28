abd15fd1689b0695c32d402ee218994c5ba501f4
Tadeu <Tadeu@192.168.1.19>
2015-10-28 15:02:18 -0200

    primeiro commit

---
+++ src/Model/Produto.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Tadeu
+ */
+public class Produto {
+
+    private String codprod;
+    private String referencia;
+    private String descricao;
+    private String codigoncm;
+    private String codnatreceita;
+    private String codtribut00;
+    private String baseicmsreg00;
+    private String aliqicmsreg00;
+    private String aliqicmspdv;
+    private String pis_cst;
+    private String cofins_cst;
+    private String aliqpis;
+    private String aliqcofins;
+    private String pisent_cst;
+    private String cofinsent_cst;
+    private String aliqpisent;
+    private String aliqcofinsent;
+
+    public String getCodprod() {
+        return codprod;
+    }
+
+    public void setCodprod(String codprod) {
+        this.codprod = codprod;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCodtribut00() {
+        return codtribut00;
+    }
+
+    public void setCodtribut00(String codtribut00) {
+        this.codtribut00 = codtribut00;
+    }
+
+    public String getCodigoncm() {
+        return codigoncm;
+    }
+
+    public void setCodigoncm(String codigoncm) {
+        this.codigoncm = codigoncm;
+    }
+
+    public String getCodnatreceita() {
+        return codnatreceita;
+    }
+
+    public void setCodnatreceita(String codnatreceita) {
+        this.codnatreceita = codnatreceita;
+    }
+
+    public String getBaseicmsreg00() {
+        return baseicmsreg00;
+    }
+
+    public void setBaseicmsreg00(String baseicmsreg00) {
+        this.baseicmsreg00 = baseicmsreg00;
+    }
+
+    public String getAliqicmsreg00() {
+        return aliqicmsreg00;
+    }
+
+    public void setAliqicmsreg00(String aliqicmsreg00) {
+        this.aliqicmsreg00 = aliqicmsreg00;
+    }
+
+    public String getAliqicmspdv() {
+        return aliqicmspdv;
+    }
+
+    public void setAliqicmspdv(String aliqicmspdv) {
+        this.aliqicmspdv = aliqicmspdv;
+    }
+
+    public String getPis_cst() {
+        return pis_cst;
+    }
+
+    public void setPis_cst(String pis_cst) {
+        this.pis_cst = pis_cst;
+    }
+
+    public String getCofins_cst() {
+        return cofins_cst;
+    }
+
+    public void setCofins_cst(String cofins_cst) {
+        this.cofins_cst = cofins_cst;
+    }
+
+    public String getAliqpis() {
+        return aliqpis;
+    }
+
+    public void setAliqpis(String aliqpis) {
+        this.aliqpis = aliqpis;
+    }
+
+    public String getAliqcofins() {
+        return aliqcofins;
+    }
+
+    public void setAliqcofins(String aliqcofins) {
+        this.aliqcofins = aliqcofins;
+    }
+
+    public String getPisent_cst() {
+        return pisent_cst;
+    }
+
+    public void setPisent_cst(String pisent_cst) {
+        this.pisent_cst = pisent_cst;
+    }
+
+    public String getCofinsent_cst() {
+        return cofinsent_cst;
+    }
+
+    public void setCofinsent_cst(String cofinsent_cst) {
+        this.cofinsent_cst = cofinsent_cst;
+    }
+
+    public String getAliqpisent() {
+        return aliqpisent;
+    }
+
+    public void setAliqpisent(String aliqpisent) {
+        this.aliqpisent = aliqpisent;
+    }
+
+    public String getAliqcofinsent() {
+        return aliqcofinsent;
+    }
+
+    public void setAliqcofinsent(String aliqcofinsent) {
+        this.aliqcofinsent = aliqcofinsent;
+    }
+    
+    
+}
